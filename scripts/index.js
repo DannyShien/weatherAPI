9303e3b87544092a8481eed7d892f5471c60f384
Stanley Chen <dannyshien@gmail.com>
2019-02-23 20:19:18 -0500

    able to grab the input

--- scripts/index.js
+++ scripts/index.js
@@ -1,55 +1,92 @@
-console.log(`You're in`);
-const URL = `https://api.openweathermap.org/data/2.5/weather?q=Atlanta&APPID=${OWKey}`;
-// const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${OWKey}`;
-const city = document.querySelector('[data-button]');
-const getTemp = document.querySelector('[data-tempDisplay]');
-const getMinTemp = document.querySelector('[data-minTemp]');
-const getMaxTemp = document.querySelector('[data-maxTemp]');
+const userInput = document.forms['form-search'];
 
-city.addEventListener('click', getWeather);
+userInput.addEventListener('submit', function(e){
+    e.preventDefault();
+    const city = userInput.querySelector('input[type="text"]').value;
+    console.log(`CITY SEARCHED: `, city)
+    // const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${OWKey}`;
+    
+    // fetch(URL)
+    // .then(r => r.json())
+    // .then(getWeather => {
+    //     console.log(getWeather)
+    // })
+});
 
-function getWeather () {
+function getWeather (userInput) {
+    console.log(userInput)
+    const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${OWKey}`;
+    
     fetch(URL)
-        .then((r) => {return r.json()})
-        .then(extractTemps)
-        .then(displayTemp)
+    .then(r => r.json())
+    .then(weatherObj => {
+        console.log(weatherObj)
+    })
 }
 
-function extractTemps (obj) {
-    // debug    ger;
-    let temp = obj.main.temp;
-    let degree = ((temp - 273.15) * 9/5 +32).toFixed(1);
 
-    let minTemp = obj.main.temp_min;
-    let minDeg = ((minTemp - 273.15) * 9/5 +32).toFixed(1);
 
-    let maxTemp = obj.main.temp_max;
-    let maxDeg = ((maxTemp - 273.15) * 9/5 +32).toFixed(1);
 
-    let temps = [];
-    temps.push(degree);
-    temps.push(minDeg);
-    temps.push(maxDeg);
 
-    return temps;
-}
+// function getWeatherInfo() {
+//     console.log('User info submitted');
 
-function displayTemp (larry) {
-    // debugger;
-    // console.log(larry);
+//     fetch(URL)
+//     .then(r => r.json())
+//     .then(weatherData => {
+//         const city = document.querySelector('[data-userInput]').value;
+//     })
+// }
 
-    let showTemp = document.createElement('li');
-    showTemp.textContent = `Current Temperature: ${larry[0]} ° F`;
 
-    let MinTemp = document.createElement('li');
-    MinTemp.textContent = `Min Temperature: ${larry[1]} ° F`;
+// const city = document.querySelector('[data-button]');
+// const getMinTemp = document.querySelector('[data-minTemp]');
+// const getMaxTemp = document.querySelector('[data-maxTemp]');
 
-    let MaxTemp = document.createElement('li');
-    MaxTemp.textContent = `Max Temperature: ${larry[2]} ° F`;
-    // console.log(showTemp);
+// city.addEventListener('click', getWeather);
 
-    getTemp.appendChild(showTemp);
-    getMinTemp.appendChild(MinTemp);
-    getMaxTemp.appendChild(MaxTemp);
-}
+// function getWeather () {
+//     fetch(URL)
+//         .then((r) => {return r.json()})
+//         .then(extractTemps)
+//         .then(displayTemp)
+// }
+
+// function extractTemps (obj) {
+//     // debug    ger;
+//     let temp = obj.main.temp;
+//     let degree = ((temp - 273.15) * 9/5 +32).toFixed(1);
+
+//     let minTemp = obj.main.temp_min;
+//     let minDeg = ((minTemp - 273.15) * 9/5 +32).toFixed(1);
+
+//     let maxTemp = obj.main.temp_max;
+//     let maxDeg = ((maxTemp - 273.15) * 9/5 +32).toFixed(1);
+
+//     let temps = [];
+//     temps.push(degree);
+//     temps.push(minDeg);
+//     temps.push(maxDeg);
+
+//     return temps;
+// }
+
+// function displayTemp (larry) {
+//     // debugger;
+//     // console.log(larry);
+
+//     let showTemp = document.createElement('li');
+//     showTemp.textContent = `Current Temperature: ${larry[0]} ° F`;
+
+//     let MinTemp = document.createElement('li');
+//     MinTemp.textContent = `Min Temperature: ${larry[1]} ° F`;
+
+//     let MaxTemp = document.createElement('li');
+//     MaxTemp.textContent = `Max Temperature: ${larry[2]} ° F`;
+//     // console.log(showTemp);
+
+//     getTemp.appendChild(showTemp);
+//     getMinTemp.appendChild(MinTemp);
+//     getMaxTemp.appendChild(MaxTemp);
+// }
 
