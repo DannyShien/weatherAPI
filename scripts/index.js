49368396d1ae20a19599c181adbcd43431c202b8
Stanley Chen <dannyshien@gmail.com>
2019-03-02 18:36:33 -0500

    now showing icon and set up format for how weather is being displayed

--- scripts/index.js
+++ scripts/index.js
@@ -4,14 +4,16 @@ const userInput = document.forms['form-search'];
 userInput.addEventListener('submit', function(e){
     e.preventDefault();
     const city = userInput.querySelector('input[type="text"]').value;
+    const zip = userInput.querySelector('input[type="text"]').value;
     // if (userInput !== city.length) {
-    //     alert('Please privide a correct input')
+    //     alert('Error, search for city not found.')
     // } else {
     //     console.log('Search accepted!');
     // }
     console.log(`CITY SEARCHED: `, city)
+    console.log(`ZIPCODE: `, zip)
     // Takes userInput(city) and places it into URL
-    const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&APPID=${OWKey}`;
+    const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&zip=${zip}&units=imperial&APPID=${OWKey}`;
     // Fetches URL with the city input
     fetch(URL)
         .then(r => r.json())
@@ -28,11 +30,11 @@ function weatherInfo(obj) {
 
     // GRABBING WEATHER
     let mainTemp = obj.main.temp
-    console.log(mainTemp)
+
     // let mainTemp = ((obj.main.temp - 273.15) * 9/5 +32).toFixed(1);
 
 
-    // GRABBING DESCRIPTION/OTHERS
+    // GRABBING DESCRIPTION & OTHERS
     let cond = obj.weather.map(condition => {return condition.main});
     let desc = obj.weather.map(description => {return description.description});
 
@@ -41,8 +43,10 @@ function weatherInfo(obj) {
     let windSpeed = (obj.wind.speed);
 
     let clouds = (obj.clouds.all)
+    console.log(clouds)
 
-    let iconCode  = obj.weather.map(img => {return img.icon})
+    let iconCode  = obj.weather.map(code => {return code.icon})
+
 
     // PUTTING VARIABLES INTO AN ARRAY
     let weather = [];
@@ -53,60 +57,76 @@ function weatherInfo(obj) {
         weather.push(cond);                     //- 4 COND
         weather.push(desc);                     //- 5 DESC
         weather.push(getIcon(iconCode));        //- 6 ICONCODE
+        weather.push(clouds);
         return weather;
 }
 
 
 function getIcon(icon) {
-    console.log(icon)
     let code = icon[0]
-    return `http://openweathermap.org/img/w/${code}.png`; //this isn't grabbing the icon
+    let iCode = `http://openweathermap.org/img/w/${code}.png`;
+    return iCode
 }
 
 
 const city = document.querySelector('[data-title]');
-const display = document.querySelector('[data-display]');
+const icon = document.querySelector('[data-icon]');
+const temp = document.querySelector('[data-temp');
+const details = document.querySelector('[data-details]');
 
 function displayWeather(weather) {
-    console.log(weather);
-
-    // Appending city name to document
+    // ==================================
+    // ===== ===== TOP HEADER ===== =====
+    // Appending CITY NAME to document
     const showName = document.createElement('h1');
     showName.textContent = weather[0];
     city.appendChild(showName);
 
-    // Appending temp to document
+    // Appending DESCRIPTION to document
+    const showDescription = document.createElement('li');
+    showDescription.textContent = `${weather[5]} `;
+    city.appendChild(showDescription);
+
+    // Appending ICON to document
+    const showIcon = document.createElement('IMG');
+    // showIcon.textContent = `${weather[6]}`;
+    showIcon.setAttribute('src', `${weather[6]}`)
+    showIcon.setAttribute('alt', 'weather icon')
+    icon.appendChild(showIcon);
+
+    // ==================================
+    // ==================================
+
+
+    // =====================================
+    // ===== ===== BOTTOM HEADER ===== =====
+    // Appending TEMP to document
     const showTemp = document.createElement('li');
-    showTemp.textContent = `${weather[1]} °`;
-    display.appendChild(showTemp);
+    showTemp.textContent = `${weather[1]} °F`;
+    temp.appendChild(showTemp);
 
-    // Appending icon to document
-    const showIcon = document.createElement('img');
-    showIcon.textContent = `${weather[6]}`;
-    display.appendChild(showIcon);
+    // Appending HUMIDITY to document 
+    const showHumidity = document.createElement('li'); 
+    showHumidity.textContent = `${weather[2]} %`;
+    details.appendChild(showHumidity);
 
-    // Appending condition to document
+    // Appending WINDSPEED to document
+    const showWindSpeed = document.createElement('li');
+    showWindSpeed.textContent = `${weather[3]} m/h`;
+    details.appendChild(showWindSpeed);
+
+    // Appdning CLOUDS to document
+    const showClouds = document.createElement('li');
+    showClouds.textContent = `${weather[7]}° cloudiness`;
+    details.appendChild(showClouds);
+
+    // Appending CONDITION to document
     // const showCondition = document.createElement('li');
     // showCondition.textContent = `${weather[4]}`;
     // display.appendChild(showCondition);
 
-    // Appending description to document
-    const showDescription = document.createElement('li');
-    showDescription.textContent = `${weather[5]} `;
-    display.appendChild(showDescription);
-
-    // Appending humidity to document 
-    const showHumidity = document.createElement('li'); 
-    showHumidity.textContent = `${weather[2]} %`;
-    display.appendChild(showHumidity);
-
-    // Appending windspeet to document
-    const showWindSpeed = document.createElement('li');
-    showWindSpeed.textContent = `${weather[3]} °`;
-    display.appendChild(showWindSpeed);
-
-
-
+    // ======================================
+    // ======================================
 }
 
 
@@ -115,25 +135,4 @@ function displayWeather(weather) {
 
 
 
-
-
-
-// function displayTemp (larry) {
-//     // debugger;
-//     // console.log(larry);
-
-//     let showTemp = document.createElement('li');
-//     showTemp.textContent = `Current Temperature: ${larry[0]} ° F`;
-
-//     let MinTemp = document.createElement('li');
-//     MinTemp.textContent = `Min Temperature: ${larry[1]} ° F`;
-
-//     let MaxTemp = document.createElement('li');
-//     MaxTemp.textContent = `Max Temperature: ${larry[2]} ° F`;
-//     // console.log(showTemp);
-
-//     getTemp.appendChild(showTemp);
-//     getMinTemp.appendChild(MinTemp);
-//     getMaxTemp.appendChild(MaxTemp);
-// }
 
